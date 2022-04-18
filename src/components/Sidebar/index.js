e7317cf67233f95f2f0d3ddbf2241c29fa89f6ef
AnassMoustaid <anassmoustaid1@gmail.com>
2022-04-18 11:43:57 +0000

    init commit

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,59 @@
+import './index.scss'
+import LogoS from '../../assets/images/am_logo.PNG'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons'
+import { faHome, faUser, faEnvelope, faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="Logo" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="work-link" to="/work">
+          <FontAwesomeIcon icon={faBriefcase} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/anass-moustaid-b92326231/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/AnassMoustaid"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ src/components/Work/index.js
@@ -0,0 +1,21 @@
+import './index.scss'
+
+
+const Work = () => {
+    return(
+       
+        <div className='work-done'>
+            <h1>Example of work done so far:  </h1>
+            <ul>
+                <li> Using Html and Bootstrap: <a href='https://anassmoustaid.github.io/bootstrap_maquette/'> https://anassmoustaid.github.io/bootstrap_maquette/</a>  </li>
+                <li> Using Anime JS Library within Javascript: <a href='https://anassmoustaid.github.io/Design_Anime_JS/'> https://anassmoustaid.github.io/Design_Anime_JS/</a>   </li>
+                <li> Using API's within Javascript: <a href='https://anassmoustaid.github.io/quote_generator/'> https://anassmoustaid.github.io/quote_generator/</a>  </li>
+                <li> Using React Framework: <a href='https://anassmoustaid.github.io/random_users/'></a> https://anassmoustaid.github.io/random_users/  </li>
+
+            </ul>    
+        </div>
+       
+    )
+}
+
+export default Work 
