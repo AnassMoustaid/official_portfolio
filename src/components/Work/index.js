c92b4d40430b5d598b40959c4119b49a5033f9bc
AnassMoustaid <anassmoustaid1@gmail.com>
2022-05-12 12:32:03 +0000

    First commit

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,23 +1,90 @@
+import { useEffect, useState } from 'react'
+import React from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 import Loader from 'react-loaders'
+import bootstrap from '../../assets/images/bootstrap_screen.PNG'
+import anime from '../../assets/images/anime_js_screen.PNG'
+import quote from '../../assets/images/quote_screen.PNG'
+import users from '../../assets/images/random_users_screen.PNG'
 
 
 const Work = () => {
-    return(
-       <>
-        <div className='work-done'>
-            <h1>Example of work done so far:  </h1>
-            <ul>
-                <li> Using Html and Bootstrap: <a href='https://anassmoustaid.github.io/bootstrap_maquette/'> https://anassmoustaid.github.io/bootstrap_maquette/</a>  </li>
-                <li> Using Anime JS Library within Javascript: <a href='https://anassmoustaid.github.io/Design_Anime_JS/'> https://anassmoustaid.github.io/Design_Anime_JS/</a>   </li>
-                <li> Using API's within Javascript: <a href='https://anassmoustaid.github.io/quote_generator/'> https://anassmoustaid.github.io/quote_generator/</a>  </li>
-                <li> Using React Framework: <a href='https://anassmoustaid.github.io/random_users/'></a> https://anassmoustaid.github.io/random_users/  </li>
+    const [letterClass, setLetterClass] = useState('text-animate')
 
-            </ul>    
-        </div>
-        <Loader type="pacman" />
-        </>
+
+    useEffect(() => {
+        return setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 2000)
+      }, [])
+
+
+    return(
+        <section className='portfolio' > 
+        <h1>
+            <AnimatedLetters
+              letterClass = {letterClass}
+              strArray={[ 'P', 'r', 'o', 'j', 'e', 'c', 't', 's', '', 'd', 'o', 'n', 'e', '', ':']}
+              idx={50}
+            /></h1>
+            <div className=" portfolio_container "> 
+                <article className="portfolio_section ">
+                
+                    <h3>Bootstrap Project</h3>
+                         <img className='image' src={bootstrap} alt="..."/>
+                         <div className="portfolio_item"> 
+                    
+                    <button className="github"><a href="https://github.com/AnassMoustaid/bootstrap_maquette"  target="blank">Github</a></button>
+                    <button className="live_demo"><a href="https://anassmoustaid.github.io/bootstrap_maquette/"  target="blank">Live Demo</a></button>
+                    </div>
+                   
+                    
+                </article>
+                <article className="portfolio_section">
+                    
+                    <h3>Quote Generator using an API:</h3>
+                         <img src={quote} alt="..."/>
+                         <div className="portfolio_item">
+                    
+                    <button className="github"><a href="https://github.com/AnassMoustaid/quote_generator"  target="blank">Github</a></button>
+                    <button className="live_demo"><a href="https://anassmoustaid.github.io/quote_generator/"  target="blank">Live Demo</a></button>
+                    </div>
+                    
+                    
+                    
+                </article>
+                <article className="portfolio_section ">
+                    
+                    <h3>Using Anime JS Library</h3>
+                         <img src={anime} alt="..."/>
+                         <div className="portfolio_item">
+                    
+                    <button className="github"><a href="https://github.com/AnassMoustaid/Design_Anime_JS"  target="blank">Github</a></button>
+                    <button className="live_demo"><a href="https://anassmoustaid.github.io/Design_Anime_JS/"  target="blank">Live Demo</a></button>
+                    </div>
+                    
+                    
+                    
+                </article>
+                <article className="portfolio_section ">
+                    
+                    <h3>Random users project using React</h3>
+                         <img src={users} alt="..."/>
+                         
+                    <div className="portfolio_item">
+                        
+                        <button className="github"><a href="https://github.com/AnassMoustaid/random_users"  target="blank">Github</a></button>
+                        <button className="live_demo"><a href=" https://anassmoustaid.github.io/random_users/"  target="blank">Live Demo</a></button>
+                        </div>
+                    
+                    
+                </article>
+                </div>
+
+                <Loader type="pacman" />
+                </section>
     )
 }
 
-export default Work 
+export default Work;
